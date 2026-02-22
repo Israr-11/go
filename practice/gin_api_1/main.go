69ada42b102daf8e0aa9370ce15ac359bed1cc59
Israr Ahmed <israrahmedpk1999@gmail.com>
2026-02-22 06:51:03 +0000

    Added initial Go module and dependencies; log router value in main.go

--- practice/gin_api_1/main.go
+++ practice/gin_api_1/main.go
@@ -1,9 +1,11 @@
 package main
+import ("fmt")
 
 import "github.com/gin-gonic/gin"
 
 func main() {
   router := gin.Default()
+  fmt.Println("The value of router %v:",router)
   router.GET("/ping", func(c *gin.Context) {
     c.JSON(200, gin.H{
       "message": "Ping Pong",
